b942ea46bb2f22162bcd5c49d38bfa198cbbb649
deathDreicer <jeferson91pineros@gmail.com>
2021-04-26 20:04:53 -0500

    fix: first commit

---
+++ src/main/java/com/skillex/proyecto/tienda/virtual/apirest/ProyectoTiendaVirtual1Application.java
@@ -0,0 +1,13 @@
+package com.skillex.proyecto.tienda.virtual.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTiendaVirtual1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTiendaVirtual1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillex/proyecto/tienda/virtual/apirest/ProyectoTiendaVirtual1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillex.proyecto.tienda.virtual.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTiendaVirtual1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
